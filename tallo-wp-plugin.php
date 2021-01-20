0951a3113d4a18e8f733c2c735aa04fd148ba98c
Guillermo Avondet dev <gavondet@gmail.com>
2021-01-20 23:51:53 +0000

    Hello, World

---
+++ tallo-wp-plugin.php
@@ -0,0 +1,14 @@
+/**
+* Plugin Name:       Talleres Online
+* Plugin URI:        https://talleresonline.net/plugins/
+* Description:       El mejor plugin para Talleres Online
+* Version:           1.1.1
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            El Despliegue
+* Author URI:        https://www.eldespliegue.com.ar/
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain:       tallo-wp-plugin
+* Domain Path:       /languages
+*/
