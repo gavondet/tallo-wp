058630056a946581f1343064509d9cb738297d47
Akio Takata <akiotakata1@gmail.com>
2021-01-22 19:19:03 +0000

    Se agrega una constante con la versión y una función que chequea la misma y la actualiza. En caso de ser distinta a la versión guardada llama a la misma acción que usamos en la activación.

--- tallo-wp-plugin.php
+++ tallo-wp-plugin.php
@@ -15,18 +15,34 @@
 * Domain Path:       /languages
 */
 
+define( 'TALLERES_ONLINE_VERSION', '1.1.1' );
+
+register_activation_hook( __FILE__, 'tallo_add_custom_roles' );
+register_deactivation_hook( __FILE__, 'tallo_remove_custom_roles' );
+
+add_action('plugins_loaded', 'tallo_check_version');
+
 
 function tallo_add_custom_roles() {
-       add_role( 'custom_role', 'Custom Subscriber', array( 'read' => true, 'level_0' => true, 'edit_posts' => true ) );
+    add_role( 'custom_role', 'Custom Subscriber', array( 'read' => true, 'level_0' => true, 'edit_posts' => true) );
+//     add_role( 'custom_role2', 'Custom Subscriber2', array( 'read' => true, 'level_0' => true) );
 }
 
 
 function tallo_remove_custom_roles() {
-       remove_role( 'custom_role', 'Custom Subscriber', array( 'read' => true, 'level_0' => true, 'edit_posts' => true ) );
+    remove_role( 'custom_role', 'Custom Subscriber', array( 'read' => true, 'level_0' => true, 'edit_posts' => true ) );
+//     remove_role( 'custom_role2', 'Custom Subscriber2', array( 'read' => true, 'level_0' => true) );
 }
 
 
+function tallo_check_version() {
+    $version = get_option('talleres_online_version');
+
+    if (TALLERES_ONLINE_VERSION !== get_option('talleres_online_version')) {
+       tallo_add_custom_roles();
+    }
+
+    update_option('talleres_online_version', TALLERES_ONLINE_VERSION);
+}
 
 
-register_activation_hook( __FILE__, 'tallo_add_custom_roles' );
-register_deactivation_hook( __FILE__, 'tallo_remove_custom_roles' );
